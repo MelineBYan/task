a18c3e500eab3fa5e3f9912894c55418995a791a
MelineBYan <77862334+MelineBYan@users.noreply.github.com>
2022-05-26 11:43:55 +0400

    Added navigateHome

--- client/src/components/form-provider/form-provider.tsx
+++ client/src/components/form-provider/form-provider.tsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Form } from 'antd';
 import { FormFinishInfo } from 'rc-field-form/lib/FormContext';
 import { FormNames } from '../../utils/constants';
 import { signIn, signUp } from '../../redux/actions';
 import { SignInData, SignUpData } from '../../core/models/user/user.model';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { RoutePath } from '../../routes/routes';
 
 /* eslint-disable-next-line */
 export interface FormProviderProps {}
 
 export const FormProvider: React.FC<FormProviderProps> = props => {
- 
+ const navigate = useNavigate();
+ const navigateHome = useCallback(() => {
+    navigate(RoutePath.WORKSPACE_CREATE);
+}, [navigate]);
     const { children } = props;
     const dispatch = useDispatch();
     const onFormFinish = (name: string, { forms, values }: FormFinishInfo) => {
         switch (name as string) {
             case FormNames.SIGN_IN: {
-                dispatch(signIn(values as SignInData));
+                dispatch(signIn({...values, navigateHome} as SignInData & {navigateHome: ()=> void}));
                 break;
             }
             case FormNames.SIGN_UP: {
                 dispatch(
-                    signUp(values as SignUpData ),
+                    signUp({...values, navigateHome}  as SignUpData  & {navigateHome: ()=> void}),
                 );
                 break;
             }
@@ -31,3 +36,4 @@ export const FormProvider: React.FC<FormProviderProps> = props => {
 };
 
 export default FormProvider;
+
